7faf8b159ddd206c00ab9a12ebde09db4fcf5da0
Daniel Radu <danielradu2107@gmail.com>
2026-09-05 20:10:10 +0300

    Edited and added some comments

--- backend/src/main/java/com/danielradu/s3/S3Service.java
+++ backend/src/main/java/com/danielradu/s3/S3Service.java
@@ -20,20 +20,23 @@ public class S3Service {
     }
 
     public void putObject(String bucketName, String key, byte[] file) {
-        // Request a specific bucket
+        // Put request object in a (specific) bucket
         PutObjectRequest objectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
                 .build();
-        // Put an object (file) inside an objectRequest
+        // Put the object inside the bucket using the PutObjectRequest
         s3Client.putObject(objectRequest, RequestBody.fromBytes(file));
     }
 
     public byte[] getObject(String bucketName, String key) throws IOException {
+        // Get request object from a (specific) bucket
         GetObjectRequest objectRequest = GetObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
                 .build();
+
+        // Return the object in array of bytes (try to) using the GetObjectRequest
          try {
              return s3Client.getObject(objectRequest).readAllBytes();
 
